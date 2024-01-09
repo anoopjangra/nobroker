4c776779e32fd792d5438323321888d0ba0b8f69
Git <anoopjangra1680@gmail.com>
2024-01-09 19:14:04 +0530

    9th jan 2024

--- src/main/java/com/nobroker/service/impl/OwnerPlanServiceImpl.java
+++ src/main/java/com/nobroker/service/impl/OwnerPlanServiceImpl.java
@@ -7,6 +7,9 @@ import com.nobroker.service.OwnerPlanService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class OwnerPlanServiceImpl implements OwnerPlanService {
 
@@ -14,16 +17,24 @@ public class OwnerPlanServiceImpl implements OwnerPlanService {
 
     private ModelMapper modelMapper;
 
-    public OwnerPlanServiceImpl(OwnerPlanRepository ownerPlanRepository) {
+    public OwnerPlanServiceImpl(OwnerPlanRepository ownerPlanRepository, ModelMapper modelMapper) {
         this.ownerPlanRepository = ownerPlanRepository;
-          this.modelMapper = modelMapper;
+        this.modelMapper = modelMapper;
     }
+
 
     @Override
     public OwnerPlanDto createOwnerPlans(OwnerPlanDto ownerPlanDto) {
         OwnerPlan ownerPlan = mapToEntity(ownerPlanDto);
         OwnerPlan savedOwnerPlan = ownerPlanRepository.save(ownerPlan);
         return mapToDto(savedOwnerPlan);
+    }
+
+    @Override
+    public List<OwnerPlanDto> getAllOwnerplans() {
+        List<OwnerPlan> ownerPlans = ownerPlanRepository.findAll();
+        List<OwnerPlanDto> ownerPlanDtos = ownerPlans.stream().map(plan -> mapToDto(plan)).collect(Collectors.toList());
+    return ownerPlanDtos;
     }
 
     OwnerPlan mapToEntity(OwnerPlanDto ownerPlanDto){
